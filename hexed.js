ae86a16867728c5b654fe772adb91fefb2c1b15f
Droid5x <mrkblnc1248@gmail.com>
2014-10-14 16:34:50 -0400

    Local change to hexed.js

--- hexed.js
+++ hexed.js
@@ -1,3 +1,9 @@
+function getSettings(item){	// Temporarily inject html for user to input settings
+	item.html('<div id="init_settings">Enter initial settings here</div>');
+}
+
+
+
 $.fn.hexed = function() {	// Begin defining main body of game
 	
 }
