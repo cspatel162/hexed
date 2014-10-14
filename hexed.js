6d7d0cef8134fa8bca50a7bd33aa01f612b79948
Daniel Ben-Chitrit <dibol13@gmail.com>
2014-10-14 16:29:45 -0400

    Added basic options
    Added options to the function

--- hexed.js
+++ hexed.js
@@ -1,3 +1,4 @@
-$.fn.hexed = function() {	// Begin defining main body of game
-	
+$.fn.hexed = function(tries, difficulty) {	// Begin defining main body of game
+	var n_tries = $.extend({10}, tries );
+	var diff = $.extend({5}, difficulty )
 }
