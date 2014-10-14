94387131ca340d5348777de24de8ba8b21a8be66
Droid5x <mrkblnc1248@gmail.com>
2014-10-14 16:35:30 -0400

    Local change to hexed.js

--- hexed.js
+++ hexed.js
@@ -1,5 +1,6 @@
 function getSettings(item){	// Temporarily inject html for user to input settings
 	item.html('<div id="init_settings">Enter initial settings here</div>');
+	$('init_settings').html();
 }
 
 
