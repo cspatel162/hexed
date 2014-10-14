b5662f2b4a75ca0330c4b50802e7086eb170c32d
Droid5x <mrkblnc1248@gmail.com>
2014-10-14 17:07:06 -0400

    Adds getColors() and percentError() functions to plugin file

--- hexed.js
+++ hexed.js
@@ -1,3 +1,21 @@
+function getColors(){		// Generates random rgb values and returns them in an array
+	var green = floor(Math.random()*255);
+	var blue = floor(Math.random()*255);
+	var red = floor(Math.random()*255);
+	return [red,green,blue];
+}
+
+function percentError(real, player){// Takes in 
+	var percentages;
+	var total = 0;
+	for (i = 0; i < 3; i++){
+		percentages[i] = (Math.abs(real[i]-player[i])/255)*100;
+		total+=percentages[i];
+	}
+	percentages[3] = total/3;
+	return percentages;
+}
+
 $.fn.hexed = function() {	// Begin defining main body of game
 	var n_tries = 10;
 	var diff = 5;
