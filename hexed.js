8cca4dd780ea698626487971ce5e208796ba4926
Daniel Ben-Chitrit <dibol13@gmail.com>
2014-10-14 16:30:06 -0400

    Update hexed.js
    Missed a semicolon

--- hexed.js
+++ hexed.js
@@ -1,4 +1,4 @@
 $.fn.hexed = function(tries, difficulty) {	// Begin defining main body of game
-	var n_tries = $.extend({10}, tries );
-	var diff = $.extend({5}, difficulty )
+	var n_tries = $.extend({10}, tries);
+	var diff = $.extend({5}, difficulty);
 }
