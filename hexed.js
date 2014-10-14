a4f48343b5e098a695920f095a88d25618263443
w-eye <coops58@gmail.com>
2014-10-14 16:46:20 -0400

    Added JQuery and Slider code

--- hexed.js
+++ hexed.js
@@ -1,4 +1,18 @@
 $.fn.hexed = function() {	// Begin defining main body of game
 	var n_tries = 10;
 	var diff = 5;
+
+	/* //Vert Slider Function
+	    $( "#slider-vertical" ).slider({
+      orientation: "vertical",
+      range: "min",
+      min: 0,
+      max: 100,
+      value: 60,
+      slide: function( event, ui ) {
+        $( "#amount" ).val( ui.value );
+      }
+    });
+    $( "#amount" ).val( $( "#slider-vertical" ).slider( "value" ) );
+ 	*/
 }
